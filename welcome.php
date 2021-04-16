b5f7ad8055f4aeb9e6ab194cc1d5fcdbe8db819c
crossingthelhx <lhx2000129@163.com>
2021-04-16 11:01:25 +0800

    test1

---
+++ welcome.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>my_php</title>
+</head>
+<body>
+<?php
+echo "你好".$_POST["fname"]."!<br/>";
+echo "你的年龄是".$_POST["age"]."岁。";
+?>
+</body>
+</html>
